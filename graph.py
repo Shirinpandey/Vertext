c1354f8c43d5a79c6a379212c7a64352c8d64ec4
unknown <shirin.pandey07@gmail.com>
2025-09-03 13:23:17 +0400

    d

--- graph.py
+++ graph.py
@@ -1,16 +1,22 @@
-from typing import Dict, TypedDict
-from langgraph.graph import StateGraph 
+from typing import Dict, TypedDict,List
+from langgraph.graph import StateGraph, START, END 
+from langchain_core import HumanMessage
+
 
 class AgentState(TypedDict):
+    value: List[int]
     name: str
+    result: str
 
-def compliment_agent(state:AgentState)-> AgentState:
-    state['name'] = f'{state['name'] } youre doing so well'
+def process_state(state:AgentState)-> AgentState:
+    '''Handle multiple inputs'''
+    state['result'] = f'Hi there {state['name'] } your sum is {sum(state["value"])}'
+
     return state
 
 
 graph = StateGraph(AgentState)
-graph.add_node('nice',compliment_agent)
+graph.add_node('nice',process_state)
 graph.set_entry_point('nice')
 graph.set_finish_point("nice")
 
--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 from langchain_ollama import OllamaLLM
 from langchain.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage,AIMessage, SystemMessage, BaseMessage,ToolMessage
+from typing import TypedDict, Annotated, Sequence
+from langgraph.graph import StateGraph, START, END 
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode
 
+class AgentState(TypedDict):
+    message: str
 
-model = OllamaLLM(model="mistral")
-template = '''
-You are a helpful assistant that translates {word} into French.
-'''
-prompt = ChatPromptTemplate.from_template(template)
+model = OllamaLLM(model="llama3.1:latest")
 
-# passing prompt in model
-chain = prompt | model 
+def process_state(state:AgentState)-> AgentState:
+    '''It will handle the request you send'''
+    # sends in the initial state of messages to the model 
+    response = model.invoke(state['message'])
+    # appends the state with the response from the model
+    state['message'].append(AIMessage(content = response.content))
+    print(response.content)
 
-while True:
-    print('-'*50)
-    print()
-    question = input("Enter your prompt (q to quit): ")
-    if question.lower() == 'q':
-        break
-    result = chain.invoke({'word': question})
-    print(result)
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node('process',process_state)
+graph.add_edge(START,'process')
+graph.add_edge("process",END)   
+agent = graph.compile()
+
+conversion_history = []
+user_input = input("Enter your prompt: ")
+# this is where you call the agent
+while user_input.lower() != "exit":
+    # you append the user input to state and pass it as message 
+    conversion_history.append(HumanMessage(content = user_input))
+    result = agent.invoke({'message': [HumanMessage(content=conversion_history)]})
+    # overwrite the variable with the new state and pass that again to be appended
+    conversion_history = result['message']
+    user_input = input("Enter your prompt: ")
